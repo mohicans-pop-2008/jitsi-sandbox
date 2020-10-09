4796f433530e4ccf9f4015bb3f5502644f9fc1b7
Chikara <chtakahashi10@gmail.com>
2020-10-09 11:27:09 -0400

    up to successfully joining conference

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
-JitsiMeetJS.init(); // how do we know this ran?
+async function jitsiInit() {
+  JitsiMeetJS.init(); // how do we know this ran?
 
-// var config = {
-//   hosts: {
-//     domain: 'meet.jit.si',
-//     muc: 'conference.' + 'meet.jit.si', // FIXME: use XEP-0030
-//     focus: 'focus.meet.jit.si',
-//   },
-//   useNicks: false,
-// };
+  console.log('connectionConfig', config)
+  // appId=null, token=null, options
+  // setting a serviceUrl on our config and specifying the use of wss instead
+  // of bosh seems to make all the red stuff go away
+  config.serviceUrl = config.websocket || config.bosh
 
-console.log('connectionConfig', config)
-// appId=null, token=null, options
-// setting a serviceUrl on our config and specifying the use of wss instead
-// of bosh seems to make all the red stuff go away
-config.serviceUrl = config.websocket || config.bosh
-const connection = new JitsiMeetJS.JitsiConnection(null, undefined, config);
+  const connection = new JitsiMeetJS.JitsiConnection(null, undefined, config);
 
-// provide a way for us to know if a connection is established,
-// failed to connect or disconnected
-const onConnectionSuccess = () => {
-  console.log('Connection Established!');
-};
+  // provide a way for us to know if a connection is established,
+  // failed to connect or disconnected
+  let conference
+  const onConnectionSuccess = () => {
+    console.log('Connection Established!');
+    conference = connection.initJitsiConference('some-default-room', {})
+  };
 
-connection.addEventListener(
-  JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED,
-  onConnectionSuccess
-);
-// connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_FAILED, onConnectionFailed);
-// connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
+  const onConnectionFailed = () => {
+    console.log('Connection Failed!')
+  }
 
-connection.connect();
+  connection.addEventListener(
+    JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED,
+    onConnectionSuccess
+  );
+
+  connection.addEventListener(
+    JitsiMeetJS.events.connection.CONNECTION_FAILED,
+    onConnectionFailed
+  );
+  // connection.addEventListener(JitsiMeetJS.events.connection.CONNECTION_DISCONNECTED, disconnect);
+
+  await connection.connect();
+
+  //ASYNCHRONOUS!
+  // const localTracks = await JitsiMeetJS.createLocalTracks({ devices: ['video', 'audio'], facingMode: 'user' }, true)
+
+  conference.on(JitsiMeetJs.events.conference.CONFERENCE_JOINED)
+  conference.join()
+}
+
+jitsiInit()
 
 const app = document.getElementById('app');
 console.log('We got the app div -->', app);
