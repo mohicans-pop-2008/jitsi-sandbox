b7eef784fd3fd053cfdc4b4750c019baa46de0b1
Kevin Hu <hukxchen+git@gmail.com>
2020-10-09 10:26:07 -0500

    feat: enable starting a conference and joining room

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
+/* global config */
+
+/* INITIALIZE */
+// what? I don't know!
 JitsiMeetJS.init(); // how do we know this ran?
 
-// var config = {
-//   hosts: {
-//     domain: 'meet.jit.si',
-//     muc: 'conference.' + 'meet.jit.si', // FIXME: use XEP-0030
-//     focus: 'focus.meet.jit.si',
-//   },
-//   useNicks: false,
-// };
+/* CONFIG */
+// what? no clue!
+// well... serviceUrl is important. See https://meet.jit.si/config.js
+// for a full config object.
 
-console.log('connectionConfig', config)
-// appId=null, token=null, options
-// setting a serviceUrl on our config and specifying the use of wss instead
-// of bosh seems to make all the red stuff go away
+//   - setting a serviceUrl on our config and specifying 
+//   the use of wss instead of bosh seems to make 
+//   all the red stuff go away
 config.serviceUrl = config.websocket || config.bosh
+
+/* ESTABLISH CONNECTION */
+// JitsiConnection(appId=null, token=undefined, options)
 const connection = new JitsiMeetJS.JitsiConnection(null, undefined, config);
 
 // provide a way for us to know if a connection is established,
 // failed to connect or disconnected
 const onConnectionSuccess = () => {
-  console.log('Connection Established!');
+  console.log(`==========Connection Established!\n
+  Go you==========
+    `);
+
+  // initJitsiConference(name, options={})
+  // REQUIRED: name
+  const room = connection.initJitsiConference('name', {});
+  room.join();
 };
+
+/* CONNECTION EVENT HANDLERS */
 
 connection.addEventListener(
   JitsiMeetJS.events.connection.CONNECTION_ESTABLISHED,
